3dbffd190580fd207e48b1323f6563e021f16e68
Matti Tahvonen <matti@vaadin.com>
2021-03-08 09:14:17 +0200

    reducing example

--- starter/src/main/java/com/example/starter/base/osgi/FixedVaadinServlet.java
+++ starter/src/main/java/com/example/starter/base/osgi/FixedVaadinServlet.java
@@ -7,27 +7,29 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletAsyncSupported;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletPattern;
 
-import com.vaadin.flow.server.VaadinServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@Component(service = Servlet.class
-/*
- * Same like @VaadinMode.
- *
- * Vaadin Servlet initial parameters can be configured as follows:
- *
- * , property = {
- * HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_INIT_PARAM_PREFIX +
- * InitParameters.SERVLET_PARAMETER_PRODUCTION_MODE + "=false"})
- */
-)
-@VaadinMode
+@Component(service = Servlet.class)
 @HttpWhiteboardServletAsyncSupported
-@HttpWhiteboardServletPattern("/*")
-public class FixedVaadinServlet extends VaadinServlet {
+@HttpWhiteboardServletPattern("/servlet/*")
+public class FixedVaadinServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Override
+    public void init() throws ServletException {
+    }
 
     @Override
-    protected void servletInitialized() throws ServletException {
-        getService().setClassLoader(getClass().getClassLoader());
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.format("<h1>Hello World!</h1>");
+        writer.format("<p>I am Servlet</p>");
     }
 
 }
